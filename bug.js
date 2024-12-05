3dc43bf4cbb4d9ac23286646787764262934b23d
BugHunter-X <askzenai@gmail.com>
2024-12-05 19:38:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* This route is causing the issue */}
+        <Route path="/contact" element={<Contact />} >
+          <Route path="/contact/details" element={<ContactDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() { return <h1>Home</h1>; }
+function About() { return <h1>About</h1>; }
+function Contact() { return <h1>Contact</h1>; }
+function ContactDetails() { return <h1>Contact Details</h1>; }
